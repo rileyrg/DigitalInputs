19a1954c111da36b27111e185cf37155c188032f
Richard Riley <rileyrg@gmail.com>
2019-06-02 16:44:52 +0200

    dir locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,7 +3,7 @@
            (progn
              (make-local-variable 'process-environment)
              (setq process-environment (copy-sequence process-environment))
-             (setenv "ARDUINO_SDK_PATH " "~/development/thirdparty/arduinoSDK"))
+             (setenv "ARDUINO_SDK_PATH" "~/development/arduino/arduinoSDK"))
 
            (projectile-project-compilation-dir . "build")
            (projectile-project-compilation-cmd . "cmake .. && make DigitalInputs_flash")
